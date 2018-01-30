9e27a2ece7ee34a853c496b57d5604ae12a4b165
snagaraj8 <sanath@gatech.edu>
2018-01-30 00:16:22 -0500

    Finished DatabaseBackend.java

--- DBTest/app/src/main/java/edu/gatech/ruddha/dbtest/DatabaseBackend.java
+++ DBTest/app/src/main/java/edu/gatech/ruddha/dbtest/DatabaseBackend.java
@@ -1,8 +1,12 @@
 package edu.gatech.ruddha.dbtest;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.HashMap;
 
 /**
  * Class representation for backend of Database
@@ -52,7 +56,6 @@ public class DatabaseBackend extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
 
     }
-
     /**
      * onUpgrade method to create db and satisfy superclass constraints. Check superclass
      * documentation for further information.
@@ -69,7 +72,6 @@ public class DatabaseBackend extends SQLiteOpenHelper {
         // Create tables again
         //onCreate(db);
     }
-
     /**
      * Private method to create a Database. Creates a single table to hold superhero data
      */
@@ -87,7 +89,6 @@ public class DatabaseBackend extends SQLiteOpenHelper {
                 KEY_PASSWORD + " TEXT," + KEY_SECRETID + " TEXT" + ")";
         db.execSQL(CREATE_PATIENTS_TABLE);
     }
-
     /**
      * Private method to clear the created table and remake it.
      */
@@ -96,24 +97,34 @@ public class DatabaseBackend extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_SUPERHEROES);
         createDB();
     }
-
     /**
      * Public method to add a superhero
      *
      * @param superhero The superhero to add to the database
      * @return whether the add was successful
-     * @throws IllegalArgumentException if superhero is already in database
      * @throws NullPointerException if superhero is null
      */
     public boolean addSuperhero(Superhero superhero) {
         String name = superhero.name;
         String password = superhero.password;
         String secretID = superhero.secretIdentity;
+        /*if (checkExists(email, TABLE_PHYSICIANS)) {
+            return false;
+        }*/
+        SQLiteDatabase db = this.getWritableDatabase();
+        // Storing values for patient
+        ContentValues newHero = new ContentValues();
+        newHero.put(KEY_NAME, name);
+        newHero.put(KEY_PASSWORD, password);
+        newHero.put(KEY_SECRETID, secretID);
+        // Inserting Row
+        db.insert(TABLE_SUPERHEROES, null, newHero);
+        db.close();
         return true;
     }
-
     /**
-     * Public method to get secret ID of superhero
+     * Public method to get secret ID of superhero. Only returns ID if the name
+     * and password are correct
      *
      * @param name the name of the superhero whose secret identity is queried
      * @param password the password of above superhero.
@@ -121,7 +132,21 @@ public class DatabaseBackend extends SQLiteOpenHelper {
      * @throws NullPointerException if name or password are null
      */
     public String attemptGetSecretID(String name, String password) {
-        return "";
+        if (name == null || password == null) {
+            throw new NullPointerException("You have entered a null name or password!");
+        }
+        SQLiteDatabase db = this.getReadableDatabase();
+        String selectQuery = "SELECT * FROM " + TABLE_SUPERHEROES;
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        // looping through all rows and adding to list
+        String secretID = null;
+        if (cursor.moveToFirst()) {
+            do {
+                if (name.equals(cursor.getString(0)) && password.equals(cursor.getString(1))) {
+                    secretID = cursor.getString(2);
+                }
+            } while (cursor.moveToNext());
+        }
+        return secretID;
     }
-
 }
