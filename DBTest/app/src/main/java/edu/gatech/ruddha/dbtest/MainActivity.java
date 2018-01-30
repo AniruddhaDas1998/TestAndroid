ae3095227e7717a54703b0aaf4594e6edaa27535
Aniruddha Das <aniruddhadas1998@gmail.com>
2018-01-30 00:41:07 -0500

    Added toast if adding fails

--- DBTest/app/src/main/java/edu/gatech/ruddha/dbtest/MainActivity.java
+++ DBTest/app/src/main/java/edu/gatech/ruddha/dbtest/MainActivity.java
@@ -56,6 +56,12 @@ public class MainActivity extends AppCompatActivity {
                     int duration = Toast.LENGTH_SHORT;
                     Toast toast = Toast.makeText(context, text, duration);
                     toast.show();
+                } else {
+                    Context context = getApplicationContext();
+                    CharSequence text = name + " already in database";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
                 }
             }
         });
