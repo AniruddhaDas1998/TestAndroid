ec8a5f8637011ae699c2bc15690573d584430032
snagaraj8 <sanath@gatech.edu>
2018-02-10 16:19:45 -0500

    Test

--- DBTest/app/src/main/java/edu/gatech/ruddha/dbtest/Superhero.java
+++ DBTest/app/src/main/java/edu/gatech/ruddha/dbtest/Superhero.java
@@ -15,6 +15,7 @@ public class Superhero {
   public Superhero(String name, String secretIdentity, String password) {
     this(name, secretIdentity, password, 21, "Planet Earth", false);
     // THIS IS A TEST OF GTENS
+    // GTENS <3
   }
 
   public Superhero(String name, String secretIdentity, String password, boolean lockedOut) {
