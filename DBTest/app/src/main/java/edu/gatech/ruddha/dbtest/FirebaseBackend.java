2b123e070a04011d86bbd989f906e091a49eb5f1
snagaraj8 <sanath@gatech.edu>
2018-02-10 18:40:39 -0500

    Updated gradle builds and added FirebaseBackend

---
+++ DBTest/app/src/main/java/edu/gatech/ruddha/dbtest/FirebaseBackend.java
@@ -0,0 +1,21 @@
+package edu.gatech.ruddha.dbtest;
+
+/**
+ * Created by Sanath on 2/10/2018.
+ */
+
+public class FirebaseBackend {
+    public boolean addUser(User user) {
+        return false;
+    }
+
+    public AccountHolder attemptLogin(String username, String password) {
+        return null;
+    }
+
+    public void resetLogins() {
+
+    }
+
+
+}
